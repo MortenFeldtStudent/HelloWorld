b89e941a56176fdadb4c3874e26d9a1941c441a3
spottieMF <cph-m227@cphbusiness.dk>
2018-11-09 09:05:35 +0100

    Start project

---
+++ src/testgithub/TestGitHub.java
@@ -0,0 +1,16 @@
+package testgithub;
+
+/**
+ *
+ * @author Morten
+ */
+public class TestGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
